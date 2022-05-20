6c01b847694c7427e38b4a58dfad8d157f151d0e
AkshayChiluveru <87972245+AkshayChiluveru@users.noreply.github.com>
2022-05-20 12:06:01 +0530

    dhanush files added

---
+++ dhanush/Primitive.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Primitive{
+
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+            float num1  = input.nextFloat();
+            float num2  = input.nextFloat();
+            float number = num1 - num2;
+            System.out.println("number is: " + number);
+        } 
+        
+
+    
+
+}
